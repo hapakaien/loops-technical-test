ded980329698e6b54807c61a2608b8f01d36672d
Ahmad Husen <husenisme@gmail.com>
2020-10-19 00:16:43 +0700

    Add observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use App\Observers\CommentObserver;
+use App\Observers\PostObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        User::observe(UserObserver::class);
+        Post::observe(PostObserver::class);
+        Comment::observe(CommentObserver::class);
     }
 }
